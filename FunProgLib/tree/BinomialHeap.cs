fc4ce4083626719ac25b94410e7f3664337e4aeb
greg <eakin@cox.net>
2014-07-23 08:32:37 -0700

    Binomial Heap

---
+++ FunProgLib/tree/BinomialHeap.cs
@@ -0,0 +1,170 @@
+﻿// Project Console Application 0.1
+// Copyright © 2014-2014. All Rights Reserved.
+// 
+// SUBSYSTEM:	FunPrograming
+// FILE:		BinomialHeap.cs
+// AUTHOR:		Greg Eakin
+namespace FunProgLib.tree
+{
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Text;
+
+    public static class BinomialHeap<T> where T : IComparable
+    {
+        private static readonly ReadOnlyCollection<Node> EmptyList = new ReadOnlyCollection<Node>(new Node[0]);
+
+        public static ReadOnlyCollection<Node> Empty
+        {
+            get
+            {
+                return EmptyList;
+            }
+        }
+
+        public class Node
+        {
+            private readonly int rank;
+
+            private readonly T root;
+
+            private readonly ReadOnlyCollection<Node> list;
+
+            public Node(int rank, T x, ReadOnlyCollection<Node> list)
+            {
+                this.rank = rank;
+                this.root = x;
+                this.list = list;
+            }
+
+            public int Rank
+            {
+                get { return rank; }
+            }
+
+            public T Root
+            {
+                get { return root; }
+            }
+
+            public ReadOnlyCollection<Node> List
+            {
+                get { return list; }
+            }
+
+            public override string ToString()
+            {
+                var result = new StringBuilder();
+                result.Append("[");
+                result.Append(root);
+                if (list.Count > 0)
+                {
+                    result.Append(", ");
+                    foreach (var node in list)
+                    {
+                        result.Append(node);
+                    }
+                }
+                result.Append("]");
+                return result.ToString();
+            }
+        }
+
+        public static bool IsEmapty(ReadOnlyCollection<Node> list)
+        {
+            return list == EmptyList;
+        }
+
+        private static ReadOnlyCollection<Node> Concatenate(Node element, ReadOnlyCollection<Node> list)
+        {
+            var x = list.ToList();
+            x.Insert(0, element);
+            return x.AsReadOnly();
+        }
+
+        private static Node Link(Node t1, Node t2)
+        {
+            if (t1.Root.CompareTo(t2.Root) <= 0) return new Node(t1.Rank + 1, t1.Root, Concatenate(t2, t1.List));
+            else return new Node(t1.Rank + 1, t2.Root, Concatenate(t1, t2.List));
+        }
+
+        private static ReadOnlyCollection<Node> InsertTree(Node t, ReadOnlyCollection<Node> ts)
+        {
+            if (ts.Count == 0) return new ReadOnlyCollection<Node>(new[] { t });
+            var tp = ts[0];
+            var tsp = ts.Skip(1).ToList().AsReadOnly();
+            if (t.Rank < tp.Rank) return Concatenate(t, ts);
+            return InsertTree(Link(t, tp), tsp);
+        }
+
+        public static ReadOnlyCollection<Node> Insert(T x, ReadOnlyCollection<Node> ts)
+        {
+            return InsertTree(new Node(0, x, EmptyList), ts);
+        }
+
+        private static ReadOnlyCollection<Node> Merge(ReadOnlyCollection<Node> ts1, ReadOnlyCollection<Node> ts2)
+        {
+            if (ts2.Count == 0) return ts1;
+            if (ts1.Count == 0) return ts2;
+
+            var t1 = ts1[0];
+            var tsp1 = ts1.Skip(1).ToList().AsReadOnly();
+
+            var t2 = ts2[0];
+            var tsp2 = ts2.Skip(1).ToList().AsReadOnly();
+
+            if (t1.Rank < t2.Rank) return Concatenate(t1, Merge(tsp1, ts2));
+            if (t2.Rank < t1.Rank) return Concatenate(t2, Merge(ts1, tsp2));
+            return InsertTree(Link(t1, t2), Merge(tsp1, tsp2));
+        }
+
+        private class Stuff
+        {
+            public Node Node { get; set; }
+
+            public ReadOnlyCollection<Node> List { get; set; }
+        }
+
+        private static Stuff RemoveMinTree(ReadOnlyCollection<Node> list)
+        {
+            if (list.Count == 0) throw new Exception("Empty");
+            if (list.Count == 1) return new Stuff { Node = list[0], List = EmptyList };
+            var t = list[0];
+            var ts = list.Skip(1).ToList().AsReadOnly();
+            var prime = RemoveMinTree(ts);
+            if (t.Root.CompareTo(prime.Node.Root) <= 0) return new Stuff() { Node = t, List = ts };
+            return new Stuff { Node = prime.Node, List = Concatenate(t, prime.List) };
+        }
+
+        public static T FindMin(ReadOnlyCollection<Node> ts)
+        {
+            var t = RemoveMinTree(ts);
+            return t.Node.Root;
+        }
+
+        public static ReadOnlyCollection<Node> DeleteMin(ReadOnlyCollection<Node> ts)
+        {
+            var t = RemoveMinTree(ts);
+            var x = t.Node.List.Reverse().ToList().AsReadOnly();
+            return Merge(x, t.List);
+        }
+
+        public static string DumpString(ReadOnlyCollection<Node> list)
+        {
+            var result = new StringBuilder();
+            result.Append("[");
+            if (list.Count > 0)
+            {
+                foreach (var node in list)
+                {
+                    result.Append(node);
+                }
+                result.Append(", ");
+            }
+            result.Remove(result.Length - 2, 2);
+            result.Append("]");
+            return result.ToString();
+        }
+    }
+}
---
+++ FunProgTests/tree/BinomialHeapTests.cs
@@ -0,0 +1,50 @@
+﻿// Project Console Application 0.1
+// Copyright © 2014-2014. All Rights Reserved.
+// 
+// SUBSYSTEM:	FunPrograming
+// FILE:		BinomialHeapTests.cs
+// AUTHOR:		Greg Eakin
+namespace FunProgTests.tree
+{
+    using System.Linq;
+
+    using FunProgLib.tree;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class BinomialHeapTests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var t = BinomialHeap<string>.Empty;
+            var x1 = BinomialHeap<string>.Insert("C", t);
+            var x2 = BinomialHeap<string>.Insert("B", x1);
+            Assert.AreEqual("[[B, [C]]]", BinomialHeap<string>.DumpString(x2));
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var t = BinomialHeap<string>.Empty;
+            var words = "what's in a name that which we call a rose by any other name would smell as sweet";
+            t = words.Split(null).Aggregate(t, (current, word) => BinomialHeap<string>.Insert(word, current));
+            Assert.AreEqual("[[as, [sweet]][a, [a, [call, [that, [which]][we]][in, [what's]][name]][name, [smell, [would]][other]][any, [by]][rose]]]", BinomialHeap<string>.DumpString(t));
+        }
+
+        [TestMethod]
+        public void DeleteMinTest()
+        {
+            var t = BinomialHeap<int>.Empty;
+            var t1 = BinomialHeap<int>.Insert(5, t);
+            var t2 = BinomialHeap<int>.Insert(3, t1);
+            var t3 = BinomialHeap<int>.Insert(6, t2);
+            var t4 = BinomialHeap<int>.DeleteMin(t3);
+            Assert.AreEqual("[[5, [6]]]", BinomialHeap<int>.DumpString(t4));
+            Assert.AreEqual(5, BinomialHeap<int>.FindMin(t4));
+
+            Assert.AreEqual(3, BinomialHeap<int>.FindMin(t3));
+        }
+    }
+}
