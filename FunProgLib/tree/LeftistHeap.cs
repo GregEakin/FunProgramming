d4ba316424fe5e318b384ad841d46f13acf4535c
greg <eakin@cox.net>
2014-07-22 22:49:41 -0700

    Add the leftest heap

---
+++ FunProgLib/tree/LeftistHeap.cs
@@ -0,0 +1,119 @@
+﻿// Project Console Application 0.1
+// Copyright © 2014-2014. All Rights Reserved.
+// 
+// SUBSYSTEM:	FunPrograming, page 20
+// FILE:		LeftistHeap.cs
+// AUTHOR:		Greg Eakin
+namespace FunProgLib.tree
+{
+    using System;
+    using System.Text;
+
+    public class LeftistHeap
+    {
+        private static readonly LeftistHeap EmptyTree = new LeftistHeap(0, 0, EmptyTree, EmptyTree);
+
+        public static LeftistHeap Empty
+        {
+            get
+            {
+                return EmptyTree;
+            }
+        }
+
+        private readonly int rank;
+
+        private readonly int min;
+
+        private readonly LeftistHeap heap1;
+
+        private readonly LeftistHeap heap2;
+
+        private LeftistHeap(int rank, int min, LeftistHeap heap1, LeftistHeap heap2)
+        {
+            this.rank = rank;
+            this.min = min;
+            this.heap1 = heap1;
+            this.heap2 = heap2;
+        }
+
+        public int Rank()
+        {
+            if (this == EmptyTree) return 0;
+            return this.rank;
+        }
+
+        private static LeftistHeap MakeT(int x, LeftistHeap a, LeftistHeap b)
+        {
+            if (a.rank >= b.rank) return new LeftistHeap(b.rank + 1, x, a, b);
+            return new LeftistHeap(a.rank + 1, x, b, a);
+        }
+
+        public bool IsEmapty
+        {
+            get
+            {
+                return this == EmptyTree;
+            }
+        }
+
+        private LeftistHeap Merge(LeftistHeap heap)
+        {
+            if (heap == EmptyTree) return this;
+            if (this == EmptyTree) return heap;
+            if (this.min <= heap.min) return MakeT(this.min, this.heap1, this.heap2.Merge(heap));
+            return MakeT(heap.min, heap.heap1, this.Merge(heap.heap2));
+        }
+
+        public LeftistHeap Insert(int x)
+        {
+            return new LeftistHeap(1, x, EmptyTree, EmptyTree).Merge(this);
+        }
+
+        public int FindMin()
+        {
+            if (this == EmptyTree)
+                throw new Exception("Empty");
+
+            return this.min;
+        }
+
+        public LeftistHeap DeleteMin()
+        {
+            if (this == EmptyTree)
+                throw new Exception("Empty");
+
+            return this.heap1.Merge(this.heap2);
+        }
+
+        public override string ToString()
+        {
+            return DumpTree(this);
+        }
+
+        private static string DumpTree(LeftistHeap tree)
+        {
+            if (tree == EmptyTree) return "\u2205";
+
+            var results = new StringBuilder();
+
+            if (tree.heap1 != EmptyTree)
+            {
+                results.Append(tree.heap1);
+            }
+
+            results.Append(tree.min);
+            //results.Append(" [");
+            //results.Append(tree.rank);
+            //results.Append("]");
+            results.Append(", ");
+
+            if (tree.heap2 != EmptyTree)
+            {
+                results.Append(tree.heap2);
+            }
+
+            return results.ToString();
+        }
+    }
+}
---
+++ FunProgTests/tree/LeftistHeapTests.cs
@@ -0,0 +1,102 @@
+﻿// Project Console Application 0.1
+// Copyright © 2014-2014. All Rights Reserved.
+// 
+// SUBSYSTEM:	FunPrograming
+// FILE:		LeftistHeapTests.cs
+// AUTHOR:		Greg Eakin
+namespace FunProgTests.tree
+{
+    using System;
+    using System.Linq;
+
+    using FunProgLib.tree;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class LeftistHeapTests
+    {
+        [TestMethod]
+        public void EmptyTest()
+        {
+            var tree = LeftistHeap.Empty;
+            Assert.AreEqual("∅", tree.ToString());
+            Assert.IsTrue(tree.IsEmapty);
+            Assert.AreEqual(0, tree.Rank());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void EmptyMinTest()
+        {
+            var tree = LeftistHeap.Empty;
+            var x = tree.FindMin();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void DeleteMinTest()
+        {
+            var tree = LeftistHeap.Empty;
+            tree = tree.DeleteMin();
+        }
+
+        [TestMethod]
+        public void SingleElement()
+        {
+            var tree = LeftistHeap.Empty;
+            tree = tree.Insert(2);
+            Assert.AreEqual("2, ", tree.ToString());
+        }
+
+        [TestMethod]
+        public void DumpTreeTest()
+        {
+            var tree = new[] { 3, 2, 5, 1 }.Aggregate(LeftistHeap.Empty, (current, word) => current.Insert(word));
+            Assert.AreEqual("3, 2, 5, 1, ", tree.ToString());
+        }
+
+        [TestMethod]
+        public void InsertTwoTest()
+        {
+            var tree = new[] { 3, 2, 5, 1 }.Aggregate(LeftistHeap.Empty, (current, word) => current.Insert(word));
+            tree = tree.Insert(4);
+            Assert.AreEqual("3, 2, 5, 1, 4, ", tree.ToString());
+        }
+
+        [TestMethod]
+        public void InsertZeroTest()
+        {
+            var tree = new[] { 3, 2, 5, 1 }.Aggregate(LeftistHeap.Empty, (current, word) => current.Insert(word));
+            tree = tree.Insert(0);
+            Assert.AreEqual("3, 2, 5, 1, 0, ", tree.ToString());
+            Assert.AreEqual(0, tree.FindMin());
+        }
+
+        [TestMethod]
+        public void MinTreeTest()
+        {
+            var tree = new[] { 3, 2, 5, 1 }.Aggregate(LeftistHeap.Empty, (current, word) => current.Insert(word));
+            Assert.AreEqual(1, tree.FindMin());
+        }
+
+        [TestMethod]
+        public void DelMinTest()
+        {
+            var tree = new[] { 3, 2, 5, 1 }.Aggregate(LeftistHeap.Empty, (current, word) => current.Insert(word));
+            tree = tree.DeleteMin();
+            Assert.AreEqual("3, 2, 5, ", tree.ToString());
+            Assert.AreEqual(2, tree.FindMin());
+        }
+
+        [TestMethod]
+        public void DelSecondMinTest()
+        {
+            var tree = new[] { 3, 2, 5, 1 }.Aggregate(LeftistHeap.Empty, (current, word) => current.Insert(word));
+            tree = tree.DeleteMin();
+            tree = tree.DeleteMin();
+            Assert.AreEqual("5, 3, ", tree.ToString());
+            Assert.AreEqual(3, tree.FindMin());
+        }
+    }
+}
