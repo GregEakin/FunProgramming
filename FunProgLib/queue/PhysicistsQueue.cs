ae6a29ffe35c4d2258052ac034b2f03798620e75
greg <eakin@cox.net>
2014-07-27 12:35:42 -0700

    Physicist's Queue

---
+++ FunProgLib/queue/PhysicistsQueue.cs
@@ -0,0 +1,47 @@
+﻿// Project Console Application 0.1
+// Copyright © 2014-2014. All Rights Reserved.
+// 
+// SUBSYSTEM:	FunPrograming
+// FILE:		PhysicistsQueue.cs
+// AUTHOR:		Greg Eakin
+namespace FunProgLib.queue
+{
+    using System;
+
+    public class PhysicistsQueue<T>
+    {
+        public class Queue
+        {
+            // alpha list
+            // int 
+            // alpha list susp
+            // int
+            // alpha list
+        }
+
+        Queue Empty
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        bool IsEmpty(Queue queue)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Queue Snoc(Queue queue, T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        T Head(Queue queue)
+        {
+            throw new NotImplementedException();
+        }
+
+        Queue Tail(Queue queue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
