5d60949e436658cadcbc0023260415b0c774d0da
greg <eakin@cox.net>
2014-07-27 12:26:11 -0700

    Queue and Deque

---
+++ FunProgLib/queue/IDeque.cs
@@ -0,0 +1,27 @@
+﻿// Project Console Application 0.1
+// Copyright © 2014-2014. All Rights Reserved.
+// 
+// SUBSYSTEM:	FunPrograming
+// FILE:		Deque.cs
+// AUTHOR:		Greg Eakin
+namespace FunProgLib.queue
+{
+    public interface IDeque<T>
+    {
+        IDeque<T> Empty { get; }
+
+        bool IsEmpty(IDeque<T> queue);
+
+        IDeque<T> Cons(IDeque<T> queue, T element);
+
+        T Head(IDeque<T> queue);
+
+        IDeque<T> Tail(IQueue<T> queue);
+
+        IDeque<T> Snoc(IDeque<T> queue, T element);
+
+        T Last(IDeque<T> queue);
+
+        IDeque<T> Init(IQueue<T> queue);
+    }
+}
---
+++ FunProgLib/queue/IQueue.cs
@@ -0,0 +1,21 @@
+﻿// Project Console Application 0.1
+// Copyright © 2014-2014. All Rights Reserved.
+// 
+// SUBSYSTEM:	FunPrograming
+// FILE:		Queue.cs
+// AUTHOR:		Greg Eakin
+namespace FunProgLib.queue
+{
+    public interface IQueue<T>
+    {
+        IQueue<T> Empty { get; }
+
+        bool IsEmpty(IQueue<T> queue);
+
+        IQueue<T> Snoc(IQueue<T> queue, T element);
+
+        T Head(IQueue<T> queue);
+
+        IQueue<T> Tail(IQueue<T> queue);
+    }
+}
