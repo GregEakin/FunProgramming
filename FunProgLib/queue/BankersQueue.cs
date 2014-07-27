1bb85f3a338bd60777933bdf8d1535ebc6d71b3e
greg <eakin@cox.net>
2014-07-27 12:31:30 -0700

    Banker's Queue

---
+++ FunProgLib/queue/BankersQueue.cs
@@ -0,0 +1,46 @@
+﻿// Project Console Application 0.1
+// Copyright © 2014-2014. All Rights Reserved.
+// 
+// SUBSYSTEM:	FunPrograming
+// FILE:		BankersQueue.cs
+// AUTHOR:		Greg Eakin
+namespace FunProgLib.queue
+{
+    using System;
+
+    public class BankersQueue<T>
+    {
+        public class Queue
+        {
+            // int
+            // alpha stream
+            // int 
+            // alpha stream
+        }
+
+        Queue Empty
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        bool IsEmpty(Queue queue)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Queue Snoc(Queue queue, T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        T Head(Queue queue)
+        {
+            throw new NotImplementedException();
+        }
+
+        Queue Tail(Queue queue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
