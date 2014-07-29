c48e863ab2a88b9ba3078236916d17509d7184a1
greg <eakin@cox.net>
2014-07-29 15:53:49 -0700

    RealTime Queue

---
+++ FunProgLib/queue/RealTimeQueue.cs
@@ -0,0 +1,41 @@
+﻿// Project Console Application 0.1
+// Copyright © 2014-2014. All Rights Reserved.
+// 
+// SUBSYSTEM:	FunPrograming
+// FILE:		RealTimeQueue.cs
+// AUTHOR:		Greg Eakin
+namespace FunProgLib.queue
+{
+    using System;
+
+    public static class RealTimeQueue<T>
+    {
+        public class Queue
+        {
+        }
+
+        private static readonly Queue EmptyQueue = null;
+
+        public static Queue Empty { get { return EmptyQueue; } }
+
+        public static bool IsEmpty(Queue q)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Queue Snoc(Queue q, T x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static T Head(Queue q)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Queue Tail(Queue q)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FunProgTests/queue/RealTimeQueueTests.cs
@@ -0,0 +1,45 @@
+﻿// Project Console Application 0.1
+// Copyright © 2014-2014. All Rights Reserved.
+// 
+// SUBSYSTEM:	FunPrograming
+// FILE:		RealTimeQueueTests.cs
+// AUTHOR:		Greg Eakin
+namespace FunProgTests.queue
+{
+    using System.Linq;
+
+    using FunProgLib.queue;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class RealTimeQueueTests
+    {
+        [Ignore]
+        [TestMethod]
+        public void EmptyTest()
+        {
+            var queue = RealTimeQueue<string>.Empty;
+            Assert.IsTrue(RealTimeQueue<string>.IsEmpty(queue));
+            queue = RealTimeQueue<string>.Snoc(queue, "Item");
+            Assert.IsFalse(RealTimeQueue<string>.IsEmpty(queue));
+            queue = RealTimeQueue<string>.Tail(queue);
+            Assert.IsTrue(RealTimeQueue<string>.IsEmpty(queue));
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void PushPopTest()
+        {
+            const string Data = "One Two Three One Three";
+            var queue = Data.Split().Aggregate(RealTimeQueue<string>.Empty, RealTimeQueue<string>.Snoc);
+
+            foreach (var expected in Data.Split())
+            {
+                var actual = RealTimeQueue<string>.Head(queue);
+                Assert.AreEqual(expected, actual);
+                queue = RealTimeQueue<string>.Tail(queue);
+            }
+        }
+    }
+}
