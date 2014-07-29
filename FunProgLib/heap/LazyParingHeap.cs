b9f248465c0f54043f5ab52ecc6d7a6f9fb47063
greg <eakin@cox.net>
2014-07-29 08:01:01 -0700

    Lazy Paring Heap

---
+++ FunProgLib/heap/LazyParingHeap.cs
@@ -0,0 +1,91 @@
+﻿// Project Console Application 0.1
+// Copyright © 2014-2014. All Rights Reserved.
+// 
+// SUBSYSTEM:	FunPrograming
+// FILE:		LazyParingHeap.cs
+// AUTHOR:		Greg Eakin
+namespace FunProgLib.heap
+{
+    using System;
+
+
+    public static class LazyParingHeap<T> where T : IComparable
+    {
+        public class Heap
+        {
+            private readonly T root;
+
+            private readonly Heap list;
+
+            private readonly Lazy<Heap> list2;  /* susp */
+
+            public Heap(T root, Heap list, Lazy<Heap> list2)
+            {
+                this.root = root;
+                this.list = list;
+                this.list2 = list2;
+            }
+
+            public T Root
+            {
+                get { return root; }
+            }
+
+            public Heap List
+            {
+                get { return list; }
+            }
+
+            public Lazy<Heap> List2
+            {
+                get { return list2; }
+            }
+        }
+
+        private static readonly Lazy<Heap> EmptyHeapSusp = /* $ */ new Lazy<Heap>(() => null);
+
+        private static readonly Heap EmptyHeap = null;
+
+        public static Heap Empty
+        {
+            get { return EmptyHeap; }
+        }
+
+        public static bool IsEmpty(Heap list)
+        {
+            return list == EmptyHeap;
+        }
+
+        public static Heap Merge(Heap h1, Heap h2)
+        {
+            if (h2 == EmptyHeap) return h1;
+            if (h1 == EmptyHeap) return h2;
+
+            if (h1.Root.CompareTo(h2.Root) <= 0) return Link(h1, h2);
+            return Link(h2, h1);
+        }
+
+        private static Heap Link(Heap h1, Heap h2)
+        {
+            if (IsEmpty(h1.List)) return new Heap(h1.Root, h2, h1.List2);
+            return new Heap(h1.Root, EmptyHeap, new Lazy<Heap>(() => Merge(Merge(h2, h1.List), h1.List2.Value)));
+        }
+
+        public static Heap Insert(T x, Heap h)
+        {
+            return Merge(new Heap(x, EmptyHeap, EmptyHeapSusp), h);
+        }
+
+        public static T FindMin(Heap h)
+        {
+            if (h == EmptyHeap) throw new Exception("Empty");
+            return h.Root;
+        }
+
+        public static Heap DeleteMin(Heap h)
+        {
+            if (h == EmptyHeap) throw new Exception("Empty");
+            return Merge(h.List, h.List2.Value);
+        }
+    }
+}
---
+++ FunProgTests/heap/LazyParingHeapTests.cs
@@ -0,0 +1,103 @@
+﻿// Project Console Application 0.1
+// Copyright © 2014-2014. All Rights Reserved.
+// 
+// SUBSYSTEM:	FunPrograming
+// FILE:		LazyParingHeapTests.cs
+// AUTHOR:		Greg Eakin
+namespace FunProgTests.heap
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using FunProgLib.heap;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class LazyParingHeapTests
+    {
+        private static string DumpHeap<T>(LazyParingHeap<T>.Heap node, bool showSusp) where T : IComparable
+        {
+            var result = new StringBuilder();
+            result.Append("[");
+            result.Append(node.Root);
+            if (!LazyParingHeap<T>.IsEmpty(node.List))
+            {
+                result.Append(", ");
+                result.Append(DumpHeap(node.List, showSusp));
+            }
+            if (showSusp || node.List2.IsValueCreated)
+            {
+                if (!LazyParingHeap<T>.IsEmpty(node.List2.Value))
+                {
+                    result.Append("; ");
+                    result.Append(DumpHeap(node.List2.Value, showSusp));
+                }
+            }
+            else
+            {
+                result.Append("; susp");
+            }
+            result.Append("]");
+            return result.ToString();
+        }
+
+        [TestMethod]
+        public void EmptyTest()
+        {
+            var t = LazyParingHeap<string>.Empty;
+            Assert.IsTrue(LazyParingHeap<string>.IsEmpty(t));
+
+            var t1 = LazyParingHeap<string>.Insert("C", t);
+            Assert.IsFalse(LazyParingHeap<string>.IsEmpty(t1));
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            var t = LazyParingHeap<string>.Empty;
+            var x1 = LazyParingHeap<string>.Insert("X", t);
+            var x2 = LazyParingHeap<string>.Insert("Y", x1);
+            // Assert.AreEqual("[X, [Y; susp]; susp]", DumpHeap(x2, false));
+            Assert.AreEqual("[X, [Y]]", DumpHeap(x2, true));
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            const string Words = "What's in a name? That which we call a rose by any other name would smell as sweet";
+            var ts = Words.Split().Aggregate(LazyParingHeap<string>.Empty, (current, word) => LazyParingHeap<string>.Insert(word, current));
+            Assert.AreEqual("[a; susp]", DumpHeap(ts, false));
+            Assert.AreEqual("[a; [a, [as, [sweet]]; [any; [by; [call; [in, [name, [rose]; [other, [smell, [would]]]]; [name?; [That, [What's]; [we, [which]]]]]]]]]]", DumpHeap(ts, true));
+        }
+
+        [TestMethod]
+        public void MergeTest()
+        {
+            const string Data1 = "What's in a name?";
+            var ts1 = Data1.Split().Aggregate(LazyParingHeap<string>.Empty, (current, word) => LazyParingHeap<string>.Insert(word, current));
+
+            const string Data2 = "That which we call a rose by any other name would smell as sweet";
+            var ts2 = Data2.Split().Aggregate(LazyParingHeap<string>.Empty, (current, word) => LazyParingHeap<string>.Insert(word, current));
+
+            var t = LazyParingHeap<string>.Merge(ts1, ts2);
+            Assert.AreEqual("[a, [a; susp]; susp]", DumpHeap(t, false));
+            Assert.AreEqual("[a, [a; [any, [as, [sweet]]; [by; [call, [name; [other, [smell, [would]]]]; [rose, [That; [we, [which]]]]]]]]; [in; [name?, [What's]]]]", DumpHeap(t, true));
+
+        }
+
+        [TestMethod]
+        public void DeleteMinTest()
+        {
+            var t = LazyParingHeap<int>.Empty;
+            var t1 = LazyParingHeap<int>.Insert(5, t);
+            var t2 = LazyParingHeap<int>.Insert(3, t1);
+            var t3 = LazyParingHeap<int>.Insert(6, t2);
+            var t4 = LazyParingHeap<int>.DeleteMin(t3);
+            Assert.AreEqual("[5, [6]]", DumpHeap(t4, false));
+            Assert.AreEqual("[5, [6]]", DumpHeap(t4, true));
+            Assert.AreEqual(5, LazyParingHeap<int>.FindMin(t4));
+
+            Assert.AreEqual(3, LazyParingHeap<int>.FindMin(t3));
+        }
+    }
+}
