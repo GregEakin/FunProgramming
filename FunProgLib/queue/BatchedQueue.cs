d4becc7056cdd59a4bfed4e1b8c26c0d16f957ab
greg <eakin@cox.net>
2014-07-28 22:02:09 -0700

    Cleanup Batched Queue

--- FunProgLib/queue/BatchedQueue.cs
+++ FunProgLib/queue/BatchedQueue.cs
@@ -7,28 +7,27 @@
 namespace FunProgLib.queue
 {
     using System;
-    using System.Collections.Generic;
-    using System.Collections.ObjectModel;
-    using System.Linq;
+
+    using FunProgLib.persistence;
 
     public static class BatchedQueue<T>
     {
         public class Queue
         {
-            private readonly ReadOnlyCollection<T> f;
-            private readonly ReadOnlyCollection<T> r;
+            private readonly LinkList<T>.List f;
+            private readonly LinkList<T>.List r;
 
-            public Queue(ReadOnlyCollection<T> f, ReadOnlyCollection<T> r)
+            public Queue(LinkList<T>.List f, LinkList<T>.List r)
             {
                 this.f = f;
                 this.r = r;
             }
 
-            public ReadOnlyCollection<T> F { get { return f; } }
-            public ReadOnlyCollection<T> R { get { return r; } }
+            public LinkList<T>.List F { get { return f; } }
+            public LinkList<T>.List R { get { return r; } }
         }
 
-        private static readonly ReadOnlyCollection<T> EmptyList = new ReadOnlyCollection<T>(new T[0]);
+        private static readonly LinkList<T>.List EmptyList = null; // new LinkList<T>.List(new T[0]);
 
         private static readonly Queue EmptyQueue = new Queue(EmptyList, EmptyList);
 
@@ -36,37 +35,30 @@ namespace FunProgLib.queue
 
         public static bool IsEmpty(Queue q)
         {
-            return q.F.Count == 0;
+            return q.F == EmptyList;
         }
 
-        private static Queue CheckF(ReadOnlyCollection<T> f, ReadOnlyCollection<T> r)
+        private static Queue CheckF(LinkList<T>.List f, LinkList<T>.List r)
         {
-            if (f.Count == 0) return new Queue(r.Reverse().ToList().AsReadOnly(), EmptyList);
+            if (f == EmptyList) return new Queue(LinkList<T>.Reverse(r), EmptyList);
             return new Queue(f, r);
-        }
-
-        private static ReadOnlyCollection<T> Concatenate(T element, IEnumerable<T> list)
-        {
-            var x = list.ToList();
-            x.Insert(0, element);
-            return x.AsReadOnly();
         }
 
         public static Queue Snoc(Queue q, T x)
         {
-            return CheckF(q.F, Concatenate(x, q.R));
+            return CheckF(q.F, LinkList<T>.Cons(x, q.R));
         }
 
         public static T Head(Queue q)
         {
-            if (q.F.Count == 0) throw new Exception("Empty");
-            return q.F[0];
+            if (q.F == EmptyList) throw new Exception("Empty");
+            return q.F.Element;
         }
 
         public static Queue Tail(Queue q)
         {
-            if (q.F.Count == 0) throw new Exception("Empty");
-            return CheckF(q.F.Skip(1).ToList().AsReadOnly(), q.R);
+            if (q.F == EmptyList) throw new Exception("Empty");
+            return CheckF(q.F.Next, q.R);
         }
     }
 }
