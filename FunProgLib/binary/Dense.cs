ea1d180b8641a242eb5cde16242f1baf0570456c
greg <eakin@cox.net>
2014-08-17 12:44:25 -0700

    Dense class

---
+++ FunProgLib/binary/Dense.cs
@@ -0,0 +1,13 @@
+﻿// Project Console Application 0.1
+// Copyright © 2014-2014. All Rights Reserved.
+// 
+// SUBSYSTEM:	FunPrograming
+// FILE:		Dense.cs
+// AUTHOR:		Greg Eakin
+namespace FunProgLib.binary
+{
+    public static class Dense
+    {
+
+    }
+}
---
+++ FunProgTests/binary/DenseTest.cs
@@ -0,0 +1,13 @@
+﻿// Project Console Application 0.1
+// Copyright © 2014-2014. All Rights Reserved.
+// 
+// SUBSYSTEM:	FunPrograming
+// FILE:		DenseTest.cs
+// AUTHOR:		Greg Eakin
+namespace FunProgTests.binary
+{
+    public class DenseTest
+    {
+         
+    }
+}
