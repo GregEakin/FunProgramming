d2b1888c3b2d0edb24c65b44492f0ec240ade10a
Greg Eakin <greg@gdbtech.info>
2016-11-27 08:46:12 -0800

    Remove coments, and temp vars

--- FunProgLib/map/Trie.cs
+++ FunProgLib/map/Trie.cs
@@ -20,24 +20,6 @@ namespace FunProgLib.map
         where K : IComparable<K>
         where T : class
     {
-        // type Key = M.Key list
-        // datatype α option = None | Some of α
-        // datatype α Map = Trie of α option x α Map M.Map
-
-        // val empty = Trie (None, M.Empty)
-
-        // fun lookup([], Trie(None, m)) = raise NotFound
-        //   | lookup([], Trie(Some x, m)) = x
-        //   | lookup(k :: ks, Trie(v, m)) = lookup(ks, M.Lookup(k, m))
-
-        // fun bind([], x, Trie(_, m)) = Trie(Some x, m)
-        //   | bind (k :: ks, x, Trie(v, m) =
-        //       let val t = M.Lookup(k, m) handle NotFound => empty
-        //           val t' = bind(ks, x, t)
-        //       in Trie(v, M.bind(k, t', m)) end
-
-        // end
-
         public sealed class Map
         {
             public Map(T v, Map m)
@@ -89,30 +71,20 @@ namespace FunProgLib.map
 
         public static Map Empty { get; } = new Map(null, null, null);
 
-        // fun lookup([], Trie(None, m)) = raise NotFound
-        //   | lookup([], Trie(Some x, m)) = x
-        //   | lookup(k :: ks, Trie(v, m)) = lookup(ks, M.Lookup(k, m))
         public static T Lookup(List<K>.Node mKey, Map trie)
         {
-            if (List<K>.IsEmpty(mKey) && trie.V == null) throw new NotFound(); // return null;  // not found
+            if (List<K>.IsEmpty(mKey) && trie.V == null) throw new NotFound();
             if (List<K>.IsEmpty(mKey)) return trie.V;
-            var xxx = Map.Lookup(List<K>.Head(mKey), trie.M);
-            return Lookup(List<K>.Tail(mKey), xxx);
+            return Lookup(List<K>.Tail(mKey), Map.Lookup(List<K>.Head(mKey), trie.M));
         }
 
-        // fun bind([], x, Trie(_, m)) = Trie(Some x, m)
-        //   | bind (k :: ks, x, Trie(v, m) =
-        //       let val t = M.Lookup(k, m) handle NotFound => empty
-        //           val t' = bind(ks, x, t)
-        //       in Trie(v, M.bind(k, t', m)) end
         public static Map Bind(List<K>.Node mKey, T x, Map trie)
         {
             if (List<K>.IsEmpty(mKey)) return new Map(x, trie.M);
             Map t;
             try { t = Map.Lookup(List<K>.Head(mKey), trie.M); } catch (NotFound) { t = Empty; }
             var tp = Bind(List<K>.Tail(mKey), x, t);
-            var xxx = Map.Bind(List<K>.Head(mKey), tp, trie.M);
-            return new Map(trie.V, xxx);
+            return new Map(trie.V, Map.Bind(List<K>.Head(mKey), tp, trie.M));
         }
     }
 }
